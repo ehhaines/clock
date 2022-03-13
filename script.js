1447827c30ce90d00209a3df40efd65cbda2fdaf
ehhaines <haines.e@northeastern.edu>
2022-03-13 05:26:41 -0400

    Implemented time keeping functionality

--- script.js
+++ script.js
@@ -7,15 +7,18 @@ ctx.fillStyle = 'rgb(0, 0, 0)';
 ctx.fillRect(0, 0, width, height);
 ctx.lineWidth = 5;
 
+let now = new Date();
+
 class Circle {
   constructor(radius, color) {
     this.radius = radius;
     this.color = color;
   }
 
-  buildCircle() {
+  buildCircle(time, units) {
     ctx.beginPath();
-    ctx.arc(0.5 * width, 0.5 * height, this.radius, 1.5 * Math.PI, 3.5 * Math.PI, false);
+    let arc_length = (time === 0) ? 3.5  * Math.PI : (1.5 * Math.PI) + (2 * Math.PI / units) * time;
+    ctx.arc(0.5 * width, 0.5 * height, this.radius, 1.5 * Math.PI, arc_length, false);
     ctx.strokeStyle = this.color;
     ctx.stroke();
   }
@@ -24,10 +27,24 @@ class Circle {
 let usable_rad = width > height ? height / 2 : width / 2;
 
 let seconds = new Circle(usable_rad - 120, '#ff0000');
-seconds.buildCircle();
+//seconds.buildCircle(now.getSeconds(), 60);
 
 let minutes = new Circle(usable_rad - 70, '#32a8a6');
-minutes.buildCircle();
+//minutes.buildCircle(now.getMinutes(), 60);
 
 let hours = new Circle(usable_rad - 20, '#98a832');
-hours.buildCircle();
+//hours.buildCircle(now.getHours(), 24);
+
+
+setInterval(() => {
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillRect(0, 0, width, height);
+  now = new Date();
+  seconds.buildCircle(now.getSeconds(), 60);
+  minutes.buildCircle(now.getMinutes(), 60);
+  hours.buildCircle(now.getHours(), 24);
+  console.log(now.getSeconds());
+  console.log(now.getMinutes());
+  console.log(now.getHours());
+}, 200);
+
