bb43f4431b77d8f278ad1b84c40515f2eaf26fce
ehhaines <haines.e@northeastern.edu>
2022-03-13 17:20:45 -0400

    Created digital clock display

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const height = canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'rgb(0, 0, 0)';
 ctx.fillRect(0, 0, width, height);
-ctx.lineWidth = 5;
+ctx.lineWidth = 7;
 
 let now = new Date();
 
@@ -17,34 +17,60 @@ class Circle {
 
   buildCircle(time, units) {
     ctx.beginPath();
-    let arc_length = (time === 0) ? 3.5  * Math.PI : (1.5 * Math.PI) + (2 * Math.PI / units) * time;
+    ctx.arc(0.5 * width, 0.5 * height, this.radius, 1.5 * Math.PI, 3.5 * Math.PI, false);
+    ctx.strokeStyle = '#808080';
+    ctx.stroke();
+    ctx.beginPath();
+    let arc_length = (1.5 * Math.PI) + (2 * Math.PI / units) * time;
     ctx.arc(0.5 * width, 0.5 * height, this.radius, 1.5 * Math.PI, arc_length, false);
     ctx.strokeStyle = this.color;
     ctx.stroke();
   }
 }
 
+class Time {
+  constructor(width) {
+    this.width = width;
+  }
+
+  displayTime(hours, min, sec) {
+    ctx.font = "64px sans-serif";
+    let text = hours.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    }) + ":" + min.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    }) + ":" + sec.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false
+    });
+    let size = ctx.measureText(text);
+    let start_x = (width / 2) - (size.width / 2);
+    ctx.strokeStyle = '#ffffff';
+    ctx.strokeText(text, start_x, height / 2, this.width);
+  }
+}
+
 let usable_rad = width > height ? height / 2 : width / 2;
 
 let seconds = new Circle(usable_rad - 120, '#ff0000');
-//seconds.buildCircle(now.getSeconds(), 60);
-
 let minutes = new Circle(usable_rad - 70, '#32a8a6');
-//minutes.buildCircle(now.getMinutes(), 60);
-
 let hours = new Circle(usable_rad - 20, '#98a832');
-//hours.buildCircle(now.getHours(), 24);
-
+let time = new Time(500);
 
 setInterval(() => {
   ctx.clearRect(0, 0, width, height);
   ctx.fillRect(0, 0, width, height);
   now = new Date();
-  seconds.buildCircle(now.getSeconds(), 60);
-  minutes.buildCircle(now.getMinutes(), 60);
-  hours.buildCircle(now.getHours(), 24);
-  console.log(now.getSeconds());
-  console.log(now.getMinutes());
-  console.log(now.getHours());
+  let sec = now.getSeconds();
+  let min = now.getMinutes();
+  let hou = now.getHours();
+  if (usable_rad - 120 >= 150) {
+    time.displayTime(hou, min, sec);
+  }
+  seconds.buildCircle(sec, 60);
+  minutes.buildCircle(min, 60);
+  hours.buildCircle(hou, 24);
 }, 200);
 
